76d4dcbcbe582569865d3a82bbbfa4377f251165
alishabohara03 <alishabohara369@gmail.com>
2025-05-22 21:27:28 +0545

    Initial commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (res,req) => {
+    
+}
+
+export const login = (res,req) => {
+    res.send("login route");
+}
+
+export const logout = (res,req) => {
+    res.send("logout route");
+}
---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+
+app.use("/api/auth",authRoutes)
+ 
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log("Server is running on PORT:" +PORT)
+    connectDB()
+}) 
