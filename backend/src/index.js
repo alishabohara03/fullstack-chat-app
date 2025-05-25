867dfaef9fb5c456092c4393ce94a7b81d918a94
alishabohara03 <alishabohara369@gmail.com>
2025-05-25 21:32:14 +0545

    login,logout,updateprofile and message

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,29 @@
-import express from "express"
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js"
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT ;
 
-app.use("/api/auth",authRoutes)
- 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log("Server is running on PORT:" +PORT)
-    connectDB()
-}) 
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on PORT: ${PORT}`);
+    });
+}).catch((error) => {
+    console.error("Failed to start server due to MongoDB connection error:", error);
+});
+
+
