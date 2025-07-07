73271a572df3fa99f57b33f0934bb953cb0c1642
alishabohara03 <alishabohara369@gmail.com>
2025-07-07 14:21:08 +0545

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,11 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT ;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +26,15 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV=="production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+ app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+  
+}
 
 
 
